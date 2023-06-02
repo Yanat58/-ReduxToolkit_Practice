56987997b76a09e5186dbe1fbb64cb582fc6c617
Yanat58 <tanyalyt80@gmail.com>
2023-06-02 15:46:00 +0300

    update

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,14 +1,13 @@
 import React from 'react'
 
-const TodoItem = () => {
-    const completed = false
+const TodoItem = ({todo}) => {    const completed = false
     return (
         <div className='flex justify-between items-center my-2'>
             <div className='text-sm px-4 py-2 cursor-pointer bg-lime-300 hover:bg-lime-400'>
-                Complete
+                {todo.complete}
             </div>
             <div className={`text-sm ${completed ? 'line-through font-medium text-lime-400' : ''}`}>
-                Todo Text
+               { todo.text}
             </div>
             <div className='text-sm px-4 py-2 flex bg-red-400 hover:bg-red-500 transition-all text-white cursor-pointer'>
                 Delete
