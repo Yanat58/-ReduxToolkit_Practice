627275f18dfa0049c1cff4e5cd4fd575a8503281
Tetiana Karlova <tanyalyt80@gmail.com>
2023-06-03 01:27:51 +0300

    update

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -1,18 +1,20 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from '@reduxjs/toolkit';
 
-const initialState={
-    todos: [],
-}
+const initialState = {
+  todos: [],
+};
 
-export const todoSlice= createSlice({
-    name: 'todos',
-    initialState,
-    reducers: {
-        addTodo: (state, action)=>{
-            state.todos.push(action.payload)
-        },
-    }
-})
+export const todoSlice = createSlice({
+  name: 'todos',
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      console.log(action);
+      console.log(action.payload);
+      state.todos.push(action.payload);
+    },
+  },
+});
 
-export const {addTodo}= todoSlice.actions;
+export const { addTodo } = todoSlice.actions;
 export default todoSlice.reducer;
